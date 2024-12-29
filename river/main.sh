e2c587ed2fffaac6a7cccd366acef226ffa980cf
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2024-12-30 00:10:34 +0700

    fix config

--- river/main.sh
+++ river/main.sh
@@ -1,5 +1,5 @@
 # pull singularity if it does not exist
-if [ ! -f ~/.river/images/singularities/images/all-spark-notebook_latest.sif ]; then
+if [ ! -f ~/.river/images/singularities/images/rstudio_latest.sif ]; then
     singularity pull --dir <<river_home>>/.river/images/singularities/images docker://docker.io/rocker/rstudio:latest 
 fi
 
@@ -7,4 +7,4 @@ fi
 echo "www-port=$PORT" >> ./analysis/river/rserver.conf
 
 # run jupyter lab
-singularity run --writable --cleanenv --env DISABLE_AUTH=true ./analysis/river/rserver.conf:/etc/rstudio/disable_auth_rserver.conf  <<river_home>>/.river/images/singularities/images/rstudio_latest.sif
+singularity run --writable --cleanenv --env DISABLE_AUTH=true -B ./analysis/river/rserver.conf:/etc/rstudio/disable_auth_rserver.conf  <<river_home>>/.river/images/singularities/images/rstudio_latest.sif
