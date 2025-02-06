6599012e724e6433812eaedb4c5a360e8d4348d5
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2025-02-06 22:42:49 +0700

    update new river config job

--- river/main.sh
+++ river/main.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-PORT=${PORT:-8787}
+while IFS== read key value; do
+    printf -v "$key" "$value"
+done < <(jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' config.json)
+
+PORT=$(python3 -c "import socket; s=socket.socket(); s.bind(('', 0)); print(s.getsockname()[1]); s.close()")
+echo $PORT > $RIVER_HOME/.river/jobs/$uuid_job_id/job.port
+echo $(hostname) > $RIVER_HOME/.river/jobs/$uuid_job_id/job.host
+echo "$uuid_job_id" > $RIVER_HOME/.river/jobs/$uuid_job_id/job.url
+echo "$uuid_job_id/" > $RIVER_HOME/.river/jobs/$uuid_job_id/job.proxy_location
+
 USER=$(whoami)
 TMPDIR=${TMPDIR:-tmp}
 CONTAINER="$RIVER_HOME/.river/images/singularities/images/rstudio-4.4.2.sif"
