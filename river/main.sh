9cd081adfe571280da8f7ad1628ad1983f001e90
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2025-02-06 23:09:01 +0700

    remove url

--- river/main.sh
+++ river/main.sh
@@ -6,7 +6,6 @@ done < <(jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' config.json)
 PORT=$(python3 -c "import socket; s=socket.socket(); s.bind(('', 0)); print(s.getsockname()[1]); s.close()")
 echo $PORT > $RIVER_HOME/.river/jobs/$uuid_job_id/job.port
 echo $(hostname) > $RIVER_HOME/.river/jobs/$uuid_job_id/job.host
-echo "$uuid_job_id" > $RIVER_HOME/.river/jobs/$uuid_job_id/job.url
 echo "$uuid_job_id/" > $RIVER_HOME/.river/jobs/$uuid_job_id/job.proxy_location
 
 USER=$(whoami)
